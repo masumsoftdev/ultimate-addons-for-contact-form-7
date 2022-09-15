5484fc2a7e0f2b1a3e2a13b88ad1649c4a8da2c6
unknown <sydurrahmant1@gmail.com>
2022-09-15 16:54:34 +0600

    v-3.1.0

--- addons/pre-populate-field/pre-populate-field.php
+++ addons/pre-populate-field/pre-populate-field.php
@@ -90,7 +90,7 @@ class UACF7_PRE_POPULATE {
                         <label for="pre_populate_enable">
                             <input class="pre-populate" id="pre_populate_enable" name="pre_populate_enable" type="checkbox" value="1" <?php checked( '1', $pre_populate_enable, true ); ?>> <?php _e( 'Enable Pre-populate fields', 'ultimate-addons-cf7' ); ?>
                         </label> 
-                        <div class="uacf7-doc-notice">Not sure how to set this? Check our step by step <a href="https://themefic.com/docs/ultimate-addons-for-contact-form-7/pre-populate-fields/" target="_blank">documentation</a>.</div>
+                        <div class="uacf7-doc-notice">Not sure how to set this? Check our step by step <a href="https://themefic.com/docs/ultimate-addons-for-contact-form-7/pre-populate-field/" target="_blank">documentation</a>.</div>
                         <?php if($pre_populate_enable != '' || $pre_populate_enable != 0): ?>
                        
                          
