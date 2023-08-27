ccfd9d8675c5c837ba86f1cc2e56630efe6afc0d
Masum Billah <office.hellomasum@gmail.com>
2023-08-27 17:07:38 +0600

    JS validation issue fix

--- addons/country-dropdown/assets/js/script.js
+++ addons/country-dropdown/assets/js/script.js
@@ -4,8 +4,8 @@
 const ds_country = document.querySelector('[ds_country="true"]');
 const ds_state = document.querySelector('[ds_state="true"]');
 const ds_city = document.querySelector('[ds_city="true"]');
-  // var dynamic_selection = $('.wpcf7-uacf7_country_dropdown').attr('dynamic-selection');
-	if(!ds_country && !ds_state && !ds_city){
+ 
+	if(!ds_country){
 		jQuery('.wpcf7-uacf7_country_dropdown').each(function(){
       var fieldId = jQuery(this).attr('id');
       var defaultCountry = jQuery(this).attr('country-code');
