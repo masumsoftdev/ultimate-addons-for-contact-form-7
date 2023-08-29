86da070e0a388f815b6f997d0a3232c5f578c99c
Masum Billah <office.hellomasum@gmail.com>
2023-08-29 11:45:11 +0600

    Loading Conditional Countries for multiple forms in single page

--- addons/country-dropdown/assets/js/script.js
+++ addons/country-dropdown/assets/js/script.js
@@ -1,38 +1,34 @@
 ;(function ($) {
 
-  
-const ds_country = document.querySelector('[ds_country="true"]');
-const ds_state = document.querySelector('[ds_state="true"]');
-const ds_city = document.querySelector('[ds_city="true"]');
+  var forms = $('.wpcf7-form');
+
+  forms.each(function(){
+
+          var formId = $(this).find('input[name="_wpcf7"]').val();
+          
+          var ds_country = $('.uacf7-form-'+formId).find('#uacf7_country_api').attr('ds_country'); 
+
  
 	if(!ds_country){
-		jQuery('.wpcf7-uacf7_country_dropdown').each(function(){
-      var fieldId = jQuery(this).attr('id');
-      var defaultCountry = jQuery(this).attr('country-code');
-      var onlyCountries = jQuery(this).attr('only-countries');  
-      if(typeof onlyCountries !== "undefined" && onlyCountries != ''){
-        onlyCountries = JSON.parse(onlyCountries);
-      }else{
-        onlyCountries = '';
-      }
-      
-      $("#"+fieldId).countrySelect({
-        defaultCountry: defaultCountry,
-        onlyCountries: onlyCountries,
-        responsiveDropdown: true,
-        preferredCountries: []
-      });
-      });
-      
-      
-      $(document).on('change', '.wpcf7-uacf7_country_dropdown', function () {
-      
-      });
-      
-	}
-
-
-  
+      $('.uacf7-form-'+formId).find('.wpcf7-uacf7_country_dropdown').each(function(){
+        var fieldId = jQuery(this).attr('id');
+        var defaultCountry = jQuery(this).attr('country-code');
+        var onlyCountries = jQuery(this).attr('only-countries');  
+        if(typeof onlyCountries !== "undefined" && onlyCountries != ''){
+          onlyCountries = JSON.parse(onlyCountries);
+        }else{
+          onlyCountries = '';
+        }
+        
+        $("#"+fieldId).countrySelect({
+          defaultCountry: defaultCountry,
+          onlyCountries: onlyCountries,
+          responsiveDropdown: true,
+          preferredCountries: []
+        });  
+      });   
+	 }
+}); 
 
 })(jQuery);
 
