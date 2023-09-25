909c3a8ca2aa4db2ec7637f98ab81bc51ccf26ed
Masum Billah <office.hellomasum@gmail.com>
2023-09-25 13:21:11 +0600

    UACF7_SIGNATURE class added

---
+++ addons/signature/inc/signature.php
@@ -0,0 +1,13 @@
+<?php
+
+// Do not access directly
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class UACF7_SIGNATURE_PANEL{
+
+}
+
+new UACF7_SIGNATURE_PANEL;
--- addons/signature/ultimate-signature.php
+++ addons/signature/ultimate-signature.php
@@ -5,3 +5,10 @@ if (!defined('ABSPATH')) {
   echo "You are not allowed to access directly";
   exit();
 }
+
+
+class UACF7_SIGNATURE{
+
+}
+
+new UACF7_SIGNATURE;
