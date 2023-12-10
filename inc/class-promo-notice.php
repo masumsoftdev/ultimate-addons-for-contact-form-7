2e615e9a0ea6084a6533cffc7d7b59795d124a20
Sydur Rahman <sydurrahmant1@gmail.com>
2023-12-10 13:27:28 +0600

    notice api update

--- inc/class-promo-notice.php
+++ inc/class-promo-notice.php
@@ -18,7 +18,7 @@ class UACF7_PROMO_NOTICE {
 
     public function __construct() {
 
-        if(in_array(date('F'), $this->months)){ 
+        if(in_array(date('F'), $this->months) && !class_exists('Ultimate_Addons_CF7_PRO')){ 
             add_filter('cron_schedules', array($this, 'uacf7_custom_cron_interval'));
         
             if (!wp_next_scheduled('uacf7_promo__schudle')) {
