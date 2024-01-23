aad543cf7f66f6f59016a9d748f50ebd65494afd
MHemelHasan <hemelsrgmts@gmail.com>
2024-01-23 12:45:08 +0600

    banner updated

--- inc/functions.php
+++ inc/functions.php
@@ -6,7 +6,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 //Require ultimate Promo Notice
 if ( file_exists( __DIR__ . '/class-promo-notice.php' ) ) {
-
 	require_once( 'class-promo-notice.php' );
 }
 
@@ -273,8 +272,6 @@ function uacf7_add_wrapper_to_cf7_form( $properties, $cfform ) {
 	return $properties;
 }
 
-
-
 // Themefic Plugin Set Admin Notice Status
 if ( ! function_exists( 'uacf7_review_activation_status' ) ) {
 
@@ -407,10 +404,18 @@ if ( ! function_exists( 'uacf7_new_updated_announcement' ) ) {
 		<div class="notice themefic_review_notice uacf7_new_updated_anno">
 			<?php echo sprintf(
 				__( '
-                        <p>
-                            Hey %1$s 👋 
-                        </p>
-                        <a class="uacf7_new_updated_anno_banner_url" target="_blank" href="https://themefic.com/uacf7-revamped-plugin-installation-and-options/"><img class="uacf7_new_updated_anno_banner" src="%2$s/uacf7_new_updated_anno.png" /></a>
+                    <a style="background-image: url(%2$s/uacf7_new_updated_anno.png)" class="uacf7_new_updated_anno_banner_url" target="_blank" href="https://themefic.com/uacf7-revamped-plugin-installation-and-options/">
+						<div class="uacf7_new_updated_anno_info_wrap">
+							<h3>Introducing UACF7 v3.3.0!</h3>
+							<p>Get ready for an exciting announcement! We will soon unveil the highly anticipated release of <b>Ultimate Addon Contact Form 7 v3.3.0</b>. Your user experience will be enhanced, and we recommend backing up your site before updating for a smooth transition</p>
+						</div>
+						<button class="uacf7_new_updated_anno_button">
+							Explore What’s New
+							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M16.0032 9.41421L7.39663 18.0208L5.98242 16.6066L14.589 8H7.00324V6H18.0032V17H16.0032V9.41421Z" fill="#382673"/>
+							</svg>
+						</button>
+					</a>
                     ', 'instantio' ),
 				$current_user->user_login, $imgurl,
 				'Instantio'
