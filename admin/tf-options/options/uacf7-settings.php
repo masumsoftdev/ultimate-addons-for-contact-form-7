67676b96f98bb97677918edb6f61fc327079dbc6
MHemelHasan <hemelsrgmts@gmail.com>
2024-02-25 18:08:57 +0600

    If the database pro addon is active, then the free addon will automatically activate by itself

--- admin/tf-options/options/uacf7-settings.php
+++ admin/tf-options/options/uacf7-settings.php
@@ -192,6 +192,7 @@ UACF7_Settings::option( 'uacf7_settings', array(
 					),
 					'uacf7_enable_database_field' => array(
 						'id' => 'uacf7_enable_database_field',
+						// 'child_field' => 'uacf7_enable_star_database_pro',
 						'type' => 'switch',
 						'label' => __( 'Database ', 'ultimate-addons-cf7' ),
 						'image_url' => UACF7_URL . 'assets/admin/images/addons/Save-to-Database.png',
@@ -203,6 +204,7 @@ UACF7_Settings::option( 'uacf7_settings', array(
 					// Added by M Hemel Hasan
 					'uacf7_enable_star_database_pro' => array(
 						'id' => 'uacf7_enable_star_database_pro',
+						'child_field' => 'uacf7_enable_database_field',
 						'type' => 'switch',
 						'label' => __( 'Database Pro', 'ultimate-addons-cf7' ),
 						'image_url' => UACF7_URL . 'assets/admin/images/addons/Save-to-Database.png',
