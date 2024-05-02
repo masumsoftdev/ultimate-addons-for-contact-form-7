77317f067e7df0aba6c8580535e51167b20a733c
MHemelHasan <hemelsrgmts@gmail.com>
2024-05-02 21:20:19 +0600

    improve setting label

--- addons/spam-protection/ultimate-spam-protection.php
+++ addons/spam-protection/ultimate-spam-protection.php
@@ -60,8 +60,8 @@ class UACF7_SPAM_PROTECTION
 					'subtitle' => __('This feature will help you to protect your form submission from Spam attacks.', 'ultimate-addons-cf7'),
 					'content' => sprintf(
 						// Translators: %1$s is replaced with the link to documentation.
-						esc_html__('Not sure how to set this? Check our step-by-step documentation on  %s .', 'ultimate-addons-cf7'),
-						'<a href="https://themefic.com/docs/uacf7/free-addons/spam-protection-for-contact-form-7/" target="_blank">Spam Protection for Contact Form 7.</a>',
+						esc_html__('Add spam protection for your contact form 7 forms. %s .', 'ultimate-addons-cf7'),
+						'<a href="https://themefic.com/docs/uacf7/free-addons/spam-protection-for-contact-form-7/" target="_blank">See Demo</a>',
 
 					),
 				),
